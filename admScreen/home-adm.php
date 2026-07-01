beb5463336a71f7af78a34a6e69352faef001a49
LucasGuiGon <lucasguidetti2008@gmail.com>
2026-07-01 17:12:36 -0300

    CSS no homeADM

--- admScreen/home-adm.php
+++ admScreen/home-adm.php
@@ -18,6 +18,7 @@ if ($userRoles['codTypeRoles'] == 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css" />
     <title>Document</title>
 </head>
 
